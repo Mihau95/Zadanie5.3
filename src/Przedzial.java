0fd860fdc6a440f5a8e1cdcd95f3bf4c62b339d7
Mihau95 <misiek0019@wp.pl>
2018-06-04 23:31:42 +0200

    Zadanie 5.3

---
+++ src/Przedzial.java
@@ -0,0 +1,18 @@
+public class Przedzial {
+    public static void main(String[] args) {
+        double i=0;
+
+        while (i<3.1){
+            System.out.printf("%.1f ", i);
+            i+=0.1;
+        }
+
+        System.out.println();
+
+        double ii=0;
+        do {
+            System.out.printf("%.1f ", ii);
+            ii+=0.1;
+        }while (ii<3.1);
+    }
+}
